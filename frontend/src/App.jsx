8f527cf34e11a476e6c826eeed0ca58b412206c4
Srushti-Kamble <kamblesrushti127@gmail.com>
2026-04-23 18:56:12 +0530

    personal details page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ShieldHerLoader from "./Pages/ShieldHerLoader";
 import AuthPages from "./pages/AuthPages";
 import { Home } from "./Pages/Home";
-
+import PersonalDetailsPage from "./Pages/PersonalDetailsPage";
 function App() {
   return (
     <BrowserRouter>
+    <PersonalDetailsPage/>
       <Routes>
      
-        <Route path="/" element={<ShieldHerLoader />} />
+        {/* <Route path="/" element={<ShieldHerLoader />} />
 
         
         <Route path="/login" element={<AuthPages />} />
 
         
-        <Route path="/home" element={<Home/>} />
+        <Route path="/home" element={<Home/>} /> */}
+
+        
       </Routes>
     </BrowserRouter>
   );
